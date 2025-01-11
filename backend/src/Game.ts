dcc190e0337c3317a5641208df7545f22c9bffe1
mitgajere <gajeramit180@gmail.com>
2025-01-11 17:14:34 +0530

    `Refactor Game class to use chess.js library and add move validation`

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,21 +1,61 @@
 import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, MOVE } from "./messages";
 
 export class Game {
-    public player1: WebSocket;
-    public player2: WebSocket;
-    private board: string;
-    private moves: string[];
-    private startTime: Date;
+  public player1: WebSocket;
+  public player2: WebSocket;
+  public board: Chess;
+  private startTime: Date;
 
-    constructor(player1: WebSocket, player2: WebSocket) {
-        this.player1 = player1;
-        this.player2 = player2;
-        this.board = "";
-        this.moves = [];
-        this.startTime = new Date();
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.startTime = new Date();
+  }
+
+  makeMove(socket: WebSocket, move: string) {
+    let from: String;
+    let to: String;
+
+    //validation of type of move using zod
+    if (this.board.moves.length % 2 === 0 && socket === this.player1) {
+      return;
+    }
+    if (this.board.moves.length % 2 === 1 && socket === this.player2) {
+      return;
     }
 
-    makeMove(socket: WebSocket, move: string) {
-        
+    try {
+      this.board.move(move);
+    } catch (e) {
+      return;
     }
+
+    if(this.board.isGameOver()){
+      this.player1.emit(JSON.stringify({
+        type: GAME_OVER,
+        payload: {
+          winner: this.board.turn() === "w" ? "black" : "white"
+        }
+      }));
+      return;
+    }
+
+    if(this.board.moves.length % 2 === 0){
+      this.player1.emit(JSON.stringify({ 
+        type: MOVE,
+        payload: move 
+      }));
+    }
+    else{
+      this.player2.emit(JSON.stringify({
+         type: MO VE,
+         payload: move
+         }
+        )
+      );
+    }
+  }
 }
